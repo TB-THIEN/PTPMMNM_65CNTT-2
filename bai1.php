4a06c8dc5808fa117122e431e6fb6f5b2b53cb57
Trần Bảo Thiên <thanhthientran969@gmail.com>
2026-09-16 15:09:04 +0700

    update bai 1

---
+++ bai1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <?php
+        $N = rand(1, 100);
+        echo "So duoc chon la: $N</p>";
+        for($i = 0; $i <= $N; $i++)
+            {
+                if($i % 2 ==0)
+                    echo "$i ";
+            }
+    ?>
+</body>
+</html>
